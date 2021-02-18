1e9744cb56c767eb9604321ff6a5090950ed3dc3
Barbara <barbaraaliverti@gmail.com>
2021-02-18 10:25:59 -0300

    Aula 5 1/2

---
+++ src/components/Link/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NextLink from 'next/link';
+
+export default function Link({  children, href, ...props }) {
+  return (
+    <NextLink href={href} passHref>
+      <a href={href}>
+        {children}
+      </a>
+    </NextLink>
+  );
+}
+ 
